088bd4321b64dd047fd248c38d0bd27dbbf6b2f2
way <1012370241@qq.com>
2023-03-14 22:30:27 +0800

    初始化+接口信息

---
+++ src/main/java/com/way/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.way.project.mapper;
+
+import com.way.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Way
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-03-14 21:40:13
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/way/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.way.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.way.project.model.entity.User;
+
+/**
+ * @Entity com.way.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
