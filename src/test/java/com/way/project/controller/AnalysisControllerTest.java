cde0f8f4ac595cc7183d9241ccb3d729abd6cc05
way <1012370241@qq.com>
2023-08-31 22:58:44 +0800

    成功上线部署

--- src/test/java/com/way/project/controller/AnalysisControllerTest.java
+++ src/test/java/com/way/project/controller/AnalysisControllerTest.java
@@ -3,6 +3,8 @@ package com.way.project.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
 
@@ -14,5 +16,19 @@ class AnalysisControllerTest {
     @Test
     void testListTopInterfaceInvoke() throws Exception {
         analysisController.listTopInterfaceInvoke();
+    }
+    
+    @Test
+    public void uriComponentsTest(){
+        String httpUrl= "http://8.130.76.170:8090/api2/poem/";
+        UriComponents components = UriComponentsBuilder.fromHttpUrl(httpUrl).build();
+        String scheme = components.getScheme();
+        System.out.println("components.getScheme() = " + scheme);
+        String host = components.getHost();
+        System.out.println("components.getHost() = " + host);
+        String path = components.getPath();
+        System.out.println("components.getPath() = " + path);
+        System.out.println(scheme+"://"+host+path);
+//        components.get
     }
 }
